2924a4ee8d62f27501d800ed9b41c5f4d4521b1f
Niti Nrw <niti.patel@flatironschool.com>
2016-11-19 11:07:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const app = "I don't do much.";
+
+//to keep the time and date
+componentWillMount: function(){
+  this.setState({ startDateTime: new Date(Date.now())});
+}
+
+componentDidMount: function(){
+  this.interval = setInterval(this.fetchWeather, 15000);
+}
+
+componentWillUnmount: function() {
+  clearInterval(this.interval);
+}
